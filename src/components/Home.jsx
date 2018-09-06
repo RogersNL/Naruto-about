f27353273b7ac4a75b7a1c9f88fd03d461d9adca
Nick Rogers and Swati Sahay <nick@>
2018-09-06 14:43:02 -0700

    Add home image.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import narutoHome from '../assets/images/narutohome.png';
 function Home(){
   return(
     <div>
+      <img src={narutoHome} />
       <style jsx>{`
         img {
           width: 100%;
-          border-radius: 60%;
-          border: 2px solid #f1f1f1;
+        }
+      `}</style>
+      <style global jsx>{`
+        body {
+          background-color: #3f53c6;
         }
       `}</style>
     </div>
